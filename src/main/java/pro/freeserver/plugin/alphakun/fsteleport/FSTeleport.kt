8a31b0a70bc16fd3d56c327330f3ba528e210271
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2023-01-02 10:51:56 +0900

    Initial commit

---
+++ src/main/java/pro/freeserver/plugin/alphakun/fsteleport/FSTeleport.kt
@@ -0,0 +1,13 @@
+package pro.freeserver.plugin.alphakun.fsteleport
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FSTeleport : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
